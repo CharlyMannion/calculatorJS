cc6b9ab89174f9eb033e8f7be40571124a20634d
Charlotte Mannion <charlottemannion100@gmail.com>
2019-11-11 12:00:36 +0000

    writes test for subtract and passes test

--- spec/calculatorSpec.js
+++ spec/calculatorSpec.js
@@ -1,9 +1,16 @@
 describe("Simple Calculator", function() {
 
-    describe("Adding", function() {
+    describe("Add", function() {
       it("Adds two numbers together", function() {
         var result = addNumbers(2, 3);
         expect(result).toEqual(5);
       });
+    });
+
+    describe("Subtract", function() {
+        it("Subtracts one number from another", function(){
+            var subtractResult = subtractNumbers(10, 5);
+            expect(subtractResult).toEqual(5);
+        });
     });
   });
--- src/calculator.js
+++ src/calculator.js
@@ -1,3 +1,7 @@
 function addNumbers(x, y) {
-    return x + y;
+    return (x + y);
+}
+
+function subtractNumbers(a, b) {
+    return (a - b);
 }
